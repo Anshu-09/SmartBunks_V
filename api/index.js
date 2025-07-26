ad7806b4cb93f02205e75a2ee76035613e74737d
Anshuman Giri <anshumangiriofficial@gmail.com>
2025-07-26 21:28:22 +0530

    vercel update

--- api/index.js
+++ api/index.js
@@ -3,19 +3,15 @@ const app = express();
 const path = require("path");
 const multer = require("multer");
 const bodyParser = require("body-parser");
+const serverless = require("serverless-http");
 
 // Middleware
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "../public")));
 app.use("/static", express.static(path.join(__dirname, "../static")));
-
-// View engine
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "../views"));
-
-
-const port = 3000;
 
 // File upload setup
 const storage = multer.diskStorage({
@@ -31,12 +27,8 @@ app.get("/", (req, res) => res.render("index", { result: null }));
 const calculateRoute = require("../api/calculate");
 app.post("/calculate", upload.single("holidayFile"), calculateRoute);
 
-app.listen(port, () => {
-  console.log(`SmartBunks server running on port ${port}`);
-});
 module.exports = app;
 
-const serverless = require("serverless-http");
 module.exports = serverless(app);
 
 
